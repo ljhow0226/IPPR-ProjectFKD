18e39d430c1bae711913b6af9a6022483f2ef9ba
user <user@DESKTOP-V9RS29L>
2023-12-27 14:27:10 +0800

    I have make a bit adjust on the input and output box in gui.fig. I have add my code into the gui.m, I have test both sammi and mine code together, all work well.

--- gui.m
+++ gui.m
@@ -115,9 +115,9 @@ switch X
 
         % Print result
         if isIdMarkPresent
-            msgbox('The currency note is Real.');
+            resultMessage = 'The currency note is Real.';
         else
-            msgbox('The currency note is Fake.');
+            resultMessage = 'The currency note is Fake.';
         end
 
     case 3
@@ -145,9 +145,9 @@ switch X
 
         % Print result
         if isIdMarkPresent
-            msgbox('The currency note is Real.');
+            resultMessage = 'The currency note is Real.';
         else
-            msgbox('The currency note is Fake.');
+            resultMessage = 'The currency note is Fake.';
         end
         
     case 4
@@ -175,9 +175,9 @@ switch X
 
         % Print result
         if isIdMarkPresent
-            msgbox('The currency note is Real.');
+            resultMessage = 'The currency note is Real.';
         else
-            msgbox('The currency note is Fake.');
+            resultMessage = 'The currency note is Fake.';
         end
 
         
@@ -206,9 +206,9 @@ switch X
 
         % Print result
         if isIdMarkPresent
-            msgbox('The currency note is Real.');
+            resultMessage = 'The currency note is Real.';
         else
-            msgbox('The currency note is Fake.');
+            resultMessage = 'The currency note is Fake.';
         end
 
         
@@ -222,6 +222,8 @@ if isequal(X,1)
     imshow(INPUTNOTE);
 else
     imshow(inputnoteEdges);
+        imshow(inputnoteEdges, []);
+    title(['Output Result: ' resultMessage]);
 end
         
   
@@ -232,7 +234,254 @@ function pushbutton3_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton3 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+global INPUTNOTE
+X = get(handles.popupmenu1,'value');
 
+switch X
+    case 2
+        % Convert image to grayscale
+        inputImgGray = rgb2gray(INPUTNOTE);
+
+        % Apply morphological operations (erosion)
+        se = strel('line', 5, 90);  % Create a vertical line structuring element
+        erodedImg = imerode(inputImgGray, se);  % Erosion using the same structuring element
+
+        % Set the region of interest (replace with your provided values)
+        x = round(1176.679);
+        y = round(133.1638);
+        width = round(64.4687);
+        height = round(743.639);
+
+        % Crop the eroded image to the selected region
+        croppedImage = imcrop(erodedImg, [x, y, width, height]);
+
+        % Create a new image with the same size as the original eroded image
+        newImage = uint8(zeros(size(erodedImg)));
+
+        % Get the size of the cropped region
+        [heightCropped, widthCropped, ~] = size(croppedImage);
+
+        % Place the cropped region in the new image
+        newImage(y:y+heightCropped-1, x:x+widthCropped-1, :) = croppedImage;
+
+        % Save the cropped image
+        imwrite(newImage, 'eroded_input.jpg');
+
+        % Load the template image for the security thread
+        templateFileName = 'template_100.jpg';
+        template = imread(templateFileName);
+
+        % Convert the template image to grayscale if it is in RGB format
+        if size(template, 3) == 3
+            templateGray = rgb2gray(template);
+        else
+            templateGray = template; % Assume the image is already in grayscale
+        end
+
+        % Convert the eroded image to grayscale
+        erodedImgGray = erodedImg;
+
+        % Perform template matching using SSD on the eroded image
+        ssdMapResult = (double(templateGray) - double(erodedImgGray)).^2;
+        ssdResult = sum(ssdMapResult(:));
+
+        % Set a threshold for matching (you can adjust this threshold)
+        realCurrencySSDResult = 53745135434;
+        threshold = 1e8; % Adjust based on the observations
+
+        % Determine whether it's a match or not
+        if abs(ssdResult - realCurrencySSDResult) < threshold
+            resultMessage = 'It is real currency!';
+        else
+            resultMessage = 'It is fake currency!';
+        end
+        
+    case 3
+        % Convert image to grayscale
+        inputImgGray = rgb2gray(INPUTNOTE);
+
+        % Apply morphological operations (erosion)
+        se = strel('line', 5, 90);  % Create a vertical line structuring element
+        erodedImg = imerode(inputImgGray, se);  % Erosion using the same structuring element
+
+        % Set the region of interest (replace with your provided values)
+        x = round(1121.2091);
+        y = round(86.6823);
+        width = round(55.4732);
+        height = round(727.1491);
+
+        % Crop the eroded image to the selected region
+        croppedImage = imcrop(erodedImg, [x, y, width, height]);
+
+        % Create a new image with the same size as the original eroded image
+        newImage = uint8(zeros(size(erodedImg)));
+
+        % Get the size of the cropped region
+        [heightCropped, widthCropped, ~] = size(croppedImage);
+
+        % Place the cropped region in the new image
+        newImage(y:y+heightCropped-1, x:x+widthCropped-1, :) = croppedImage;
+
+        % Save the cropped image
+        imwrite(newImage, 'eroded_input.jpg');
+
+        % Load the template image for the security thread
+        templateFileName = 'template_200.jpg';
+        template = imread(templateFileName);
+
+        % Convert the template image to grayscale if it is in RGB format
+        if size(template, 3) == 3
+            templateGray = rgb2gray(template);
+        else
+            templateGray = template; % Assume the image is already in grayscale
+        end
+
+        % Convert the eroded image to grayscale
+        erodedImgGray = erodedImg;
+
+        % Perform template matching using SSD on the eroded image
+        ssdMapResult = (double(templateGray) - double(erodedImgGray)).^2;
+        ssdResult = sum(ssdMapResult(:));
+
+        % Set a threshold for matching (you can adjust this threshold)
+        realCurrencySSDResult = 73882821251;
+        threshold = 1e8; % Adjust based on the observations
+
+        % Determine whether it's a match or not
+        if abs(ssdResult - realCurrencySSDResult) < threshold
+            resultMessage = 'It is real currency!';
+        else
+            resultMessage = 'It is fake currency!';
+        end
+        
+    case 4
+        % Convert image to grayscale
+        inputImgGray = rgb2gray(INPUTNOTE);
+
+        % Apply morphological operations (erosion)
+        se = strel('line', 5, 90);  % Create a vertical line structuring element
+        erodedImg = imerode(inputImgGray, se);  % Erosion using the same structuring element
+
+        % Set the region of interest (replace with your provided values)
+        x = round(1124.75);
+        y = round(136.25);
+        width = round(88.5);
+        height = round(708);
+        
+        % Crop the eroded image to the selected region
+        croppedImage = imcrop(erodedImg, [x, y, width, height]);
+
+        % Create a new image with the same size as the original eroded image
+        newImage = uint8(zeros(size(erodedImg)));
+
+        % Get the size of the cropped region
+        [heightCropped, widthCropped, ~] = size(croppedImage);
+
+        % Place the cropped region in the new image
+        newImage(y:y+heightCropped-1, x:x+widthCropped-1, :) = croppedImage;
+
+        % Save the cropped image
+        imwrite(newImage, 'eroded_input.jpg');
+
+        % Load the template image for the security thread
+        templateFileName = 'template_500.jpg';
+        template = imread(templateFileName);
+
+        % Convert the template image to grayscale if it is in RGB format
+        if size(template, 3) == 3
+            templateGray = rgb2gray(template);
+        else
+            templateGray = template; % Assume the image is already in grayscale
+        end
+
+        % Convert the eroded image to grayscale
+        erodedImgGray = erodedImg;
+
+        % Perform template matching using SSD on the eroded image
+        ssdMapResult = (double(templateGray) - double(erodedImgGray)).^2;
+        ssdResult = sum(ssdMapResult(:));
+
+        % Set a threshold for matching (you can adjust this threshold)
+        realCurrencySSDResult = 63531498076;
+        threshold = 1e8; % Adjust based on the observations
+
+        % Determine whether it's a match or not
+        if abs(ssdResult - realCurrencySSDResult) < threshold
+            resultMessage = 'It is real currency!';
+        else
+            resultMessage = 'It is fake currency!';
+        end
+        
+    case 5
+        % Convert image to grayscale
+        inputImgGray = rgb2gray(INPUTNOTE);
+
+        % Apply morphological operations (erosion)
+        se = strel('line', 5, 90);  % Create a vertical line structuring element
+        erodedImg = imerode(inputImgGray, se);  % Erosion using the same structuring element
+
+        % Set the region of interest (replace with your provided values)
+        x = round(1308.9737);
+        y = round(127.07127);
+        width = round(88.4813);
+        height = round(734.8448);
+
+        % Crop the eroded image to the selected region
+        croppedImage = imcrop(erodedImg, [x, y, width, height]);
+
+        % Create a new image with the same size as the original eroded image
+        newImage = uint8(zeros(size(erodedImg)));
+
+        % Get the size of the cropped region
+        [heightCropped, widthCropped, ~] = size(croppedImage);
+
+        % Place the cropped region in the new image
+        newImage(y:y+heightCropped-1, x:x+widthCropped-1, :) = croppedImage;
+
+        % Save the cropped image
+        imwrite(newImage, 'eroded_input.jpg');
+
+        % Load the template image for the security thread
+        templateFileName = 'template_2000.jpg';
+        template = imread(templateFileName);
+
+        % Convert the template image to grayscale if it is in RGB format
+        if size(template, 3) == 3
+            templateGray = rgb2gray(template);
+        else
+            templateGray = template; % Assume the image is already in grayscale
+        end
+
+        % Convert the eroded image to grayscale
+        erodedImgGray = erodedImg;
+
+        % Perform template matching using SSD on the eroded image
+        ssdMapResult = (double(templateGray) - double(erodedImgGray)).^2;
+        ssdResult = sum(ssdMapResult(:));
+
+        % Set a threshold for matching (you can adjust this threshold)
+        realCurrencySSDResult = 77879205449;
+        threshold = 1e8; % Adjust based on the observations
+
+        % Determine whether it's a match or not
+        if abs(ssdResult - realCurrencySSDResult) < threshold
+            resultMessage = 'It is real currency!';
+        else
+            resultMessage = 'It is fake currency!';
+        end
+ 
+    otherwise
+        msgbox("Please select correct note.")
+end
+
+axes(handles.axes3);
+
+if isequal(X,1)
+    imshow(INPUTNOTE);
+else
+    imshow(ssdMapResult, []);
+    title(['SSD Map (Result: ' num2str(ssdResult) ') - ' resultMessage]);
+end
 
 % --- Executes on selection change in popupmenu1.
 function popupmenu1_Callback(hObject, eventdata, handles)
